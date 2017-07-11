122bca6ae11dd137720f91267ecac50ad3a62b28
Barce <james.barcelona@accuenmedia.com>
2017-07-11 13:42:50 -0700

    get_profiles()

--- amazonadapi/amazonclient.py
+++ amazonadapi/amazonclient.py
@@ -9,7 +9,7 @@ import jwt
 import requests
 import time
 import os
-
+import sys
 
 use_environment_variables = None
 
@@ -43,7 +43,7 @@ class AmazonClient:
   def connect(self):
     get_token_url = "https://api.amazon.com/auth/o2/token"
     payload = "grant_type=authorization_code&code=" + self.amzn_code + "&redirect_uri=https%3A//www.accuenplatform.com/accounts/login/%3Fnext%3D/backstage/api/advertiser&client_id=" + self.client_id + "&client_secret=" + self.client_secret
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': "Basic " + self.base64auth().decode('utf-8')}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     print(get_token_url)
     print(payload)
     print(headers)
@@ -65,7 +65,17 @@ class AmazonClient:
 
     print("Auth code, {}, entered.".format(self.amzn_code))
     self.raw_token_results = self.connect()
-    print("raw_token_results:")
-    print(self.raw_token_results)
-    self.token = ''
+    # print("raw_token_results:")
+    # print(self.raw_token_results)
+    self.token = self.raw_token_results['access_token']
+    return self.token
         
+  # curl -X GET -H "Content-Type:application/json" -H "Authorization: Bearer $AMZN_TOKEN" https://advertising-api.amazon.com/v1/profiles
+  def get_profiles(self):
+    url = "https://advertising-api.amazon.com/v1/profiles"
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.token}
+    r = requests.get(url, headers=headers)
+    results_json = r.json()
+    return results_json
+      
+

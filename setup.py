ee1c9a94f4cff886f72c67045ff612bad7e6c666
Barce <james.barcelona@accuenmedia.com>
2017-07-12 16:36:49 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amazonadapi',
-    version='0.0.3',
+    version='0.0.4',
     author='Jim Barcelona, Arun Suresh',
     author_email='barce@me.com, arunvsuresh@gmail.com',
     packages=['amazonadapi', 'amazonadapi.tests'],

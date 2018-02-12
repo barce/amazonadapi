952da813894615dd230908244053be681ecfee68
Arun Suresh <arunvsuresh@gmail.com>
2018-02-12 10:08:19 -0500

    refactor 'get_line_items'

--- amazonadapi/amazonclient.py
+++ amazonadapi/amazonclient.py
@@ -359,23 +359,60 @@ class AmazonClient:
         self.page_token = matches[0]
 
       results_json = r.json()
-      json_ids = results_json['object']['objects']
-      for json_id in json_ids:
-        print(json_id)
-        ids.append(json_id['id']['value'])
 
-      try:
-        if 'error' in results_json:
-          self.error_check_json(results_json)
-          return results_json
-      except Exception as e:
-        print("expected result")
-        return e
+      # error checking json return
+      if 'error' in results_json:
+        # update access token
+        self.token = self.error_check_json(results_json)['access_token']
+        # apply headers with new token, return response and response dict
+        r, results_json = self.make_new_request(url, self.token)
+
+        ids = self.get_order_campaign_ids(results_json, ids)
+
+        # use results_json to create updated json dict
+        response_json = self.generate_json_response(r, results_json)
+        # generate list of campaign ids and add to data object
+        response_json['data']['ids'] = ids
+        # delete unnecessary info
+        del response_json['data']['object']
+
+        self.page_token = None
+        self.page_size = None
+
+      else:
+        ids = self.get_order_campaign_ids(results_json, ids)
+        response_json = self.generate_json_response(r, results_json)
+        # generate list of campaign ids and add to data object
+        response_json['data']['ids'] = ids
+        # delete unnecessary info
+        del response_json['data']['object']
+
+        self.page_token = None
+        self.page_size = None
+
+      return json.dumps(response_json)
 
 
-    self.page_token = None
-    self.page_size = None
-    return ids
+
+
+
+      # json_ids = results_json['object']['objects']
+      # for json_id in json_ids:
+      #   print(json_id)
+      #   ids.append(json_id['id']['value'])
+      #
+      # try:
+      #   if 'error' in results_json:
+      #     self.error_check_json(results_json)
+      #     return results_json
+      # except Exception as e:
+      #   print("expected result")
+      #   return e
+
+
+    # self.page_token = None
+    # self.page_size = None
+    # return ids
 
   def get_line_item(self, line_item_id):
     url = "https://" + self.host + "/da/v1/line-items/" + line_item_id
